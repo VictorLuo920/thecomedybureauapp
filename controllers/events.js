d10d57585cfd888afcc0c97cd82e298d04792e03
Victor Luo <victorluo@MacBook-Air.local>
2020-04-17 08:55:56 -0700

    before expiring deadline commit, cleaned up, made notes on where to leave off, functions will work when update argument is properly formatted

--- controllers/events.js
+++ controllers/events.js
@@ -3,6 +3,7 @@ const rootURL = 'https://app.ticketmaster.com/discovery/v2/events?apikey=aAdFKyb
 const Event = require('../models/event');
 const User = require('../models/user')
 
+// this loads the events page: first it makes a request to the API to retrieve those JSON packs, parses them, uses a for loop to iterate over each received package and magically copying a version of the data onto my own database! the power of findOneandUpdate with upsert...
 const index = (req, res, next) => {
   request(rootURL, (err, response, body) => {
     const eventsData = JSON.parse(body);
@@ -20,6 +21,7 @@ const index = (req, res, next) => {
   });
 }
 
+// this bookmarks the event to associate the Eventid with the user's Bookmarked events field so that the user had their own reference point to make notes on the event
 const bookmark = (req, res, next) => {
   Event.findOne({ticketmasterId: req.params.id}, (err, event) => {
     User.findById(req.user._id, (err, userData) => {
--- controllers/users.js
+++ controllers/users.js
@@ -2,13 +2,14 @@ const User = require('../models/user');
 const Event = require('../models/event');
 
 
+// shows the user profile page, making sure bookmarked events are properly referenced with .populate()
 const show = (req, res, next) => {
   User.findById(req.user._id).populate('bookmarkedEvents').exec((err, user) => {
     res.render("profile", {user})
   })
 };
 
-
+// adds a comment and specifying its numerous UX restrictions: so that the comment is properly referenced to its specific bookmarked Event. 
 const createNote = (req, res, next) => {
   Event.findOne({_id: req.params.id}, (err, event) => {
     User.findById(req.user._id, (err, userData) => {
@@ -20,6 +21,7 @@ const createNote = (req, res, next) => {
   res.redirect("/profile");
 };
 
+// directs users to the comment edit page
 const edit = (req, res, next) => {
   User.findById(req.user._id)
     .populate('bookmarkedEvents').exec(function(err, user) {
@@ -27,6 +29,7 @@ const edit = (req, res, next) => {
   });
 };
 
+// should be saving the edits that are loaded into the inputs, but figuring out how to push the req.body (text) into the text: string value in my notes schema...
 const update = (req, res, next) => {
   User.findByIdAndUpdate(
     req.user._id,
@@ -38,8 +41,17 @@ const update = (req, res, next) => {
   );
 };
 
-
-const deleteNote = () => {};
+// should be tied to an inline button that displays on the profile page that simply redirects or refreshes the profile page to show that the changes were made
+const deleteNote = (req, res, next) => {
+  User.findByIdAndUpdate(
+    req.user._id,
+    req.body, // this update function should target the specific comment ID and splice it out of the array. would adding options help? 
+    (err, updatedUser) => {
+      console.log(req.user._id);
+      res.redirect("/profile");
+    }
+  );
+};
 
 
 module.exports = {
